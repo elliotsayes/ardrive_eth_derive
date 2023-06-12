90b2d0809615c6c9789c5789ac6e5da1dc06f9e1
Elliot Sayes <elliot@sayes.cloud>
2023-06-13 11:16:35 +1200

    Hash signature for correct entropy size

--- lib/crypto/eth_derive.dart
+++ lib/crypto/eth_derive.dart
@@ -4,15 +4,22 @@ import 'dart:typed_data';
 import 'package:arweave/arweave.dart';
 import 'package:bip39/bip39.dart' as bip39;
 import 'package:convert/convert.dart';
+import 'package:cryptography/cryptography.dart';
 
-Future<String> ethSignMessage(Wallet wallet, String message) async {
+// ardrive-web/lib/services/crypto/crypto.dart
+final sha256 = Sha256();
+
+Future<Uint8List> ethSignMessage(Wallet wallet, String message) async {
   final messageData = Uint8List.fromList(utf8.encode(message));
   final walletSignature = await wallet.sign(messageData);
-  final walletSignatureHex = hex.encode(walletSignature);
-  return walletSignatureHex;
+  // final walletSignatureHex = hex.encode(walletSignature);
+  return walletSignature;
 }
 
-Future<String> signatureToBip39Mnemonnic(String signatureHex) async {
+Future<String> signatureToBip39Mnemonnic(Uint8List signature) async {
+  final signatureSha256 = await sha256.hash(signature);
+  final signatureSha256Data = Uint8List.fromList(signatureSha256.bytes);
+  final signatureHex = hex.encode(signatureSha256Data);
   final bip39Mnemonnic = bip39.entropyToMnemonic(signatureHex);
   return bip39Mnemonnic;
 }
