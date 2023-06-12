f23bb812be0df48e2434fe38166a2cc654ffd4dd
Elliot Sayes <elliot@sayes.cloud>
2023-06-13 11:02:02 +1200

    Add signing message function

--- lib/crypto/eth_derive.dart
+++ lib/crypto/eth_derive.dart
@@ -1,10 +1,23 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:arweave/arweave.dart';
 import 'package:bip39/bip39.dart' as bip39;
+import 'package:convert/convert.dart';
+
+Future<String> ethSignMessage(Wallet wallet, String message) async {
+  final messageData = Uint8List.fromList(utf8.encode(message));
+  final walletSignature = await wallet.sign(messageData);
+  final walletSignatureHex = hex.encode(walletSignature);
+  return walletSignatureHex;
+}
 
 Future<String> signatureToBip39Mnemonnic(String signatureHex) async {
-  return bip39.entropyToMnemonic(signatureHex);
+  final bip39Mnemonnic = bip39.entropyToMnemonic(signatureHex);
+  return bip39Mnemonnic;
 }
 
 Future<Wallet> bip39MnemonnicToWallet(String bip39Mnemonnic) async {
-  return await Wallet.createWalletFromMnemonic(bip39Mnemonnic);
+  final wallet = await Wallet.createWalletFromMnemonic(bip39Mnemonnic);
+  return wallet;
 }
